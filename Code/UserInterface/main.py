74723bf9a6fb10c0d200b12c75e67d6dc74b0147
Frederik Peetz-Schou Larsen <s174159@student.dtu.dk>
2023-11-15 11:17:07 +0100

    Basic streamlit with eulearian magnification

--- Code/UserInterface/main.py
+++ Code/UserInterface/main.py
@@ -4,9 +4,10 @@ import numpy as np
 import cv2
 
 st.set_page_config(page_title="Eulerian Magnification", page_icon=":eyeglasses:")
-tab1, tab2 = st.tabs(["Main", "Background"])
+tab1, tab2, tab3 = st.tabs(["Pre-recorded video", "Live feed", "About"])
 
 
+## Helper functions ""
 def markdownreader(file):
     with open("Text/" + file) as f:
         lines = f.readlines()
@@ -14,6 +15,7 @@ def markdownreader(file):
             st.markdown(line, unsafe_allow_html=True)
 
 
+## TABS ##
 with tab1:
     markdownreader("Main.md")
     # Data uploade
@@ -26,4 +28,28 @@ with tab1:
 
 
 with tab2:
+    # Init
+    markdownreader("Webcam.md")
+    start_button_pressed = st.button("Start")
+
+    stop_bottom_pressed = st.button("Stop")
+
+    frame_placeholder = st.empty()
+
+    if start_button_pressed:
+        cap = cv2.VideoCapture(0)
+        while cap.isOpened() and not stop_bottom_pressed:
+            ret, frame = cap.read()
+            if not ret:
+                st.write("The video capture has ended")
+                break
+            frame = cv2.cvtColor(
+                frame, cv2.COLOR_BGR2RGB
+            )  # RGB Format to support streamlit
+            frame_placeholder.image(frame, channels="RGB")
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+with tab3:
     markdownreader("Background.md")
